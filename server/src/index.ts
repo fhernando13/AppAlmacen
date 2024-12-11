50703d2bd1a7f154d18f15307c138960c3a1d68c
fhers <fhers667@gmail.com>
2024-12-10 21:54:42 -0600

    configuraciones

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 // Rutas
 import indexRoutes from './routes/indexRoutes';
@@ -15,7 +17,10 @@ class Server{
 
     config(): void{
         this.app.set('port', process.env.PORT || 3000);
-
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
     }
 
     routes(): void{
